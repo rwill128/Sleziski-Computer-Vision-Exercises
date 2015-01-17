95a0da8349e6d27f39d06250001dbe3c544c776d
rwill128 <rwill128.create@gmail.com>
2015-01-17 14:31:42 -0600

    Adding stuff.

--- src/image/imageUtil.js
+++ src/image/imageUtil.js
@@ -1,5 +1,17 @@
 var ImageUtil = {
-    loadImage: function(elementId, width, height) {
-
+    loadImage: function(path, width, height, callback) {
+        var canvas = document.createElement('canvas');
+        Caman(canvas, path, function() {
+            var pixelData = [];
+            for (var i = 0; i < height; i++) {
+                pixelData.push([]);
+                if((i*j) % 3 !== 0) {
+                    for (var j = 0; j < width; j++) {
+                        pixelData[i].push(this.originalPixelData[i * j] * .299 + this.originalPixelData[(i * j) + 1] * .587 + this.originalPixelData[(i * j) + 2] * .114)
+                    }
+                }
+            }
+            callback(pixelData)
+        })
     }
 };
--- test/image/imageUtilSpec.js
+++ test/image/imageUtilSpec.js
@@ -1,7 +1,37 @@
 describe('The ImageUtil API', function() {
-    it('should load an image from the specified image path.', function() {
-        var image = ImageUtil.loadImage('data/blue-square', 2, 2)
-        dump(image)
+    it('should load one pixel correctly from an image at the specified image path.', function(done) {
+        var callback = function(pixelData) {
+            expect(pixelData).toEqual([[83.77]]);
+            done()
+        };
+        ImageUtil.loadImage('data/blue-square.jpg', 1, 1, callback)
+    });
+    it('should load 2 horizontal pixels correctly from an image.', function(done) {
+        var callback = function(pixelData) {
+            expect(pixelData).toEqual([[83.77, 83.77]]);
+            done()
+        };
+        ImageUtil.loadImage('data/blue-square.jpg', 2, 1, callback)
+    });
+    it('should load 2 vertical pixels correctly from an image.', function(done) {
+        var callback = function(pixelData) {
+            expect(pixelData).toEqual([[83.77], [83.77]]);
+            done()
+        };
+        ImageUtil.loadImage('data/blue-square.jpg', 1, 2, callback)
+    });
+    it('should load 3 horizonal pixels correctly from an image.', function(done) {
+        var callback = function(pixelData) {
+            expect(pixelData).toEqual([[83.77, 83.77, 83.77]]);
+            done()
+        };
+        ImageUtil.loadImage('data/blue-square.jpg', 3, 1, callback)
+    });
+    it('should load 4 horizonal pixels correctly from an image.', function(done) {
+        var callback = function(pixelData) {
+            expect(pixelData).toEqual([[83.77, 83.77, 83.77, 83.77]]);
+            done()
+        };
+        ImageUtil.loadImage('data/blue-square.jpg', 4, 1, callback)
     })
-    
 });
